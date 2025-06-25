5bc53b2e4de9d3b202884707350544164ae42433
Ghaid0904 <wi23b192@technikum-wien.at>
2025-06-25 02:55:57 +0200

    communityDepleted & gridPortion Attribute hinzugefügt

--- rest-api/src/main/java/at/powergrid/dto/EnergyData.java
+++ rest-api/src/main/java/at/powergrid/dto/EnergyData.java
@@ -7,6 +7,9 @@ public class EnergyData {
     private double percentage;
     private double produced_kWh;
     private double used_kWh;
+    private double communityDepleted;
+    private double gridPortion;
+
 
     public EnergyData() {
         // notwendig für Jackson
@@ -17,12 +20,26 @@ public class EnergyData {
         this.percentage = percentage;
     }
 
-    public EnergyData(OffsetDateTime timestamp, double percentage, double produced_kWh, double used_kWh, double communityDepleted, double gridPortion) {
+    public EnergyData(OffsetDateTime timestamp, double percentage, double produced_kWh, double used_kWh, double gridPortion) {
         this.timestamp = timestamp;
         this.percentage = percentage;
         this.produced_kWh = produced_kWh;
         this.used_kWh = used_kWh;
     }
+    public EnergyData(OffsetDateTime timestamp, double communityDepleted, double gridPortion) {
+        this.timestamp = timestamp;
+        this.communityDepleted = communityDepleted;
+        this.gridPortion = gridPortion;
+    }
+    public EnergyData(OffsetDateTime timestamp, double percentage, double produced_kWh, double used_kWh, double communityDepleted, double gridPortion) {
+        this.timestamp = timestamp;
+        this.percentage = percentage;
+        this.produced_kWh = produced_kWh;
+        this.used_kWh = used_kWh;
+        this.communityDepleted = communityDepleted;
+        this.gridPortion = gridPortion;
+    }
+
 
     public OffsetDateTime getTimestamp() {
         return timestamp;
@@ -56,5 +73,23 @@ public class EnergyData {
     public void setPercentage(double percentage) {
         this.percentage = percentage;
     }
+
+
+    public double getCommunityDepleted() {
+        return communityDepleted;
+    }
+
+    public void setCommunityDepleted(double communityDepleted) {
+        this.communityDepleted = communityDepleted;
+    }
+
+    public double getGridPortion() {
+        return gridPortion;
+    }
+
+    public void setGridPortion(double gridPortion) {
+        this.gridPortion = gridPortion;
+    }
+
 
 }
